ae575550fdfcaadc71bcedbf473525cb6fa8509f
CJS <CJS@DESKTOP-IMRPC4F>
2018-07-26 18:20:36 +0900

    news img 저장 코딩완료

--- ex00/src/main/java/com/budongsan/news/craw/NewsCraw.java
+++ ex00/src/main/java/com/budongsan/news/craw/NewsCraw.java
@@ -17,7 +17,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class NewsCraw {
-	
 
 	ArrayList<String> title = new ArrayList<String>();
 	ArrayList<String> content = new ArrayList<String>();
@@ -25,10 +24,8 @@ public class NewsCraw {
 	ArrayList<String> writeDate = new ArrayList<String>();
 	ArrayList<String> lastWriteDate = new ArrayList<String>();
 	ArrayList<String> img = new ArrayList<String>();
-//	ArrayList<Date> writeDate = new ArrayList<Date>();
-	
-	
-	
+	// ArrayList<Date> writeDate = new ArrayList<Date>();
+
 	Map<String, ArrayList<String>> result = new HashMap<String, ArrayList<String>>();
 	Map<String, ArrayList<String>> lastResult = new HashMap<String, ArrayList<String>>();
 	Map<String, ArrayList<Date>> result2 = new HashMap<String, ArrayList<Date>>();
@@ -36,68 +33,56 @@ public class NewsCraw {
 	private String pageUrl = "http://realestate.daum.net/news/all?page=";
 
 	public Map<String, ArrayList<String>> craw() throws IOException, ParseException, IndexOutOfBoundsException {
-		for (int i = 3; i <= 3; i++) {
+		for (int i = 1; i <= 1; i++) {
 			Document doc = Jsoup.connect(pageUrl + i).get();
-			//1.제목
-			Elements eTilte=doc.select("#live > ul > li > div > strong a");
-				for (Element et1 : eTilte) {
-					title.add(et1.text());
-					System.out.println(et1.text());
-				}
-			
+			// 1.제목
+			Elements eTilte = doc.select("#live > ul > li > div > strong a");
+			for (Element et1 : eTilte) {
+				title.add(et1.text());
+				System.out.println(et1.text());
+			}
 
 			// 2.컨텐트
 			Elements eClink = doc.select("#live > ul > li > a");
-				for (Element el2 : eClink) {
-					Document doc2 = Jsoup.connect(el2.attr("abs:href")).get();
-					Elements eContent = doc2.select("#dmcfContents > section");
+			for (Element el2 : eClink) {
+				Document doc2 = Jsoup.connect(el2.attr("abs:href")).get();
+				Elements eContent = doc2.select("#dmcfContents > section");
 
-					for (Element et2 : eContent) {	
-						content.add(et2.text());
-						System.out.println("=============================");
-						System.out.println(et2.text());
-				} 
-			} 
+				for (Element et2 : eContent) {
+					content.add(et2.text());
+					System.out.println("=============================");
+					System.out.println(et2.text());
+				}
+			}
 
 			// 3.작성자
-			
-				Elements eWriter = doc.select("#live > ul > li > div > p > span > span.source");
-				
-				for (Element et3 : eWriter) {
-					writer.add(et3.text());
-				
-			}
-			//4.작성일
-			
-				Elements eWriteDate = doc.select("#live > ul > li > div > p > span > span.time");
-				
-				for(Element et4:eWriteDate) {
-					writeDate.add(et4.text());
-				}
-			
-			
-				String abz="http://realestate.daum.net/news/all?page=1";
-				
-				//이미지
-				Elements ilink = doc.select("#live > ul > li > a");
-				for (Element el5 : ilink) {
-					Document doc5 = Jsoup.connect(el5.attr("abs:href")).get();
-				Elements eImg = doc5.select("#dmcfContents > section > figure > img");
-	
-				for(Element et5:eImg) {
-//					if(!(et5.attr("abs:src").equals(abz))) {
-//						System.out.println(et5.attr("abs:src"));
-					img.add(et5.attr("abs:src"));
-					}
-//				else {
-					img.add("https://www.popit.kr/wp-content/uploads/2018/07/vpKKHrD.jpg");
-				
-//					}	
-//				}
-				}
-				
-				
 
+			Elements eWriter = doc.select("#live > ul > li > div > p > span > span.source");
+
+			for (Element et3 : eWriter) {
+				writer.add(et3.text());
+
+			}
+			// 4.작성일
+
+			Elements eWriteDate = doc.select("#live > ul > li > div > p > span > span.time");
+
+			for (Element et4 : eWriteDate) {
+				writeDate.add(et4.text());
+			}
+
+
+			// 이미지
+			Elements ilink = doc.select("#live > ul > li > a");
+			for (Element el5 : ilink) {
+				Document doc5 = Jsoup.connect(el5.attr("abs:href")).get();
+				Elements eImg = doc5.select("#dmcfContents > section > figure > img");
+				if(eImg.size() == 0) {
+					img.add("https://t1.daumcdn.net/daumtop_chanel/op/20170315064553027.png");
+				} else {
+					img.add(eImg.get(0).attr("abs:src"));
+				}
+			}
 
 			
 			result.put("title", title);
@@ -105,44 +90,22 @@ public class NewsCraw {
 			result.put("writer", writer);
 			result.put("writeDate", writeDate);
 			result.put("img", img);
-			
 
 		} // page for문 종료
 
 		return result;
-		
+
 	}
-//	public Map<String, ArrayList<Date>> crawDate() throws IOException, ParseException {
-//		SimpleDateFormat sdf = new SimpleDateFormat("YYYY.MM.dd hh:mm");
-//		for (int i = 1; i <= 1; i++) {
-//			Document doc = Jsoup.connect(pageUrl + i).get();
-//			//4.작성일
-//				Elements eWriteDate = doc.select("#content > div.view_area > p > span.date");
-//				for(Element et4 : eWriteDate) {		
-//						wD = et4.text();
-//						writeDate.add(sdf.parse(wD));
-//						System.out.println(sdf.parse(wD));
-//
-//				}
-//				
-//				
-//		
-//				result2.put("writeDate", writeDate);
-//			}
-//			
-//		
-//		return result2;
-//
-//	}
-	public Map<String, ArrayList<String>> last() throws IOException{
+
+
+	public Map<String, ArrayList<String>> last() throws IOException {
 		Document doc = Jsoup.connect(pageUrl + "1").get();
 		Elements LWriteDate = doc.select("#live > ul > li:nth-child(1)> div > p > span > span.time");
-		for(Element etl4:LWriteDate) {
+		for (Element etl4 : LWriteDate) {
 			lastWriteDate.add(etl4.text());
 		}
 		lastResult.put("lastWriteDate", lastWriteDate);
-		
+
 		return lastResult;
 	}
 }
-
--- ex00/src/main/java/com/budongsan/news/dao/NewsDAO.java
+++ ex00/src/main/java/com/budongsan/news/dao/NewsDAO.java
@@ -63,13 +63,12 @@ public class NewsDAO {
 		ArrayList<String> title = new ArrayList<String>();
 		ArrayList<String> content = new ArrayList<String>();
 		ArrayList<String> writer = new ArrayList<String>();
-//		ArrayList<Date> writeDate = new ArrayList<Date>();
 		ArrayList<String> writeDate = new ArrayList<String>();
 		ArrayList<String> last = new ArrayList<String>();
 		ArrayList<String> img = new ArrayList<String>();
 		Map<String, ArrayList<String>> map = myCraw.craw();
 		Map<String, ArrayList<String>> mapp = myCraw.last();
-//		Map<String, ArrayList<Date>> mapp = myCraw.crawDate();
+
 				
 		title = map.get("title");
 		content = map.get("content");
@@ -77,36 +76,21 @@ public class NewsDAO {
 		writeDate = map.get("writeDate");
 		last = mapp.get("lastWriteDate");
 		img = map.get("img");
-//		writeDate = mapp.get("writeDate");
-//		wD = map.get("writeDate");
+
 		
 		System.out.println("============================================");
 		System.out.println(title.size());
 		System.out.println(content.size());
 		System.out.println(writer.size());
 		System.out.println(writeDate.size());
-		System.out.println(last.size());
 		System.out.println(img.size());
-	
 
-//		for(int i=0; i<20; i++) {
-//			System.out.println(i + "t:" + title.get(i));}
-//		for(int i=0; i<20; i++) {
-//			System.out.println(i + "c:" + content.get(i));}
-//		for(int i=0; i<20; i++) {
-//			System.out.println(i + "w:" + writer.get(i));}
-//		for(int i=0; i<20; i++) {
-//			System.out.println(i + "wd:" + writeDate.get(i));}
-		
-//		int a = writeDate.size()-1; //20개
-//		boolean b =(writeDate.get(a) != last.get(0)); //마지막 writeDate와 lastDate가 같지않으면  lastDate 위치까지 insert
-//		if (b=true) {
 	
 		int b = writeDate.size();
 			for (int i = 0; i < title.size(); i++) {
 				NewsDTO dto = new NewsDTO(title.get(i),content.get(i), writer.get(i), writeDate.get(i),img.get(i));
 				sql.insert(abs + "craw", dto);	
-				System.out.println("--------**"+img.get(i));
+			
 			}
 				}
 			}				
